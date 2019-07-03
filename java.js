6c7f256b217f33834729b4a1ae16cc7a7ae7a430
ugramildehyde <52052421+ugramildehyde@users.noreply.github.com>
2019-07-03 13:25:24 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+window.alert('prepare to be disapponited');
+// function bigImg1(x) {
+//   x.style.height = "600px";
+//   x.style.width = "600px";
+// }
+//
+// function normalImg1(x) {
+//   x.style.height = "508px";
+//   x.style.width = "508px";
+// }
+// function bigImg2(x) {
+//   x.style.height = "600px";
+//   x.style.width = "600px";
+// }
+//
+// function normalImg2(x) {
+//   x.style.height = "508px";
+//   x.style.width = "512px";
+// }
